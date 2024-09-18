1ddee2c603b00ee174fabd0bac2e231f327cc402
YoungEunLee <61973941+cnsejr2@users.noreply.github.com>
2024-09-19 01:23:20 +0900

    Title: 행복한 수열의 개수, Time: 154ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240919/행복한 수열의 개수/number-of-happy-sequence.java
@@ -0,0 +1,56 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        // 여기에 코드를 작성해주세요.
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+
+        int[][] arr = new int[n][n];
+        for (int i = 0; i < n; i++) {
+            st = new StringTokenizer(br.readLine());
+            for (int j = 0; j < n; j++) {
+                arr[i][j] = Integer.parseInt(st.nextToken());
+            }
+        }
+        
+        int sum = 0;
+        for (int i = 0; i < n; i++) {
+        	int cnt = 1;
+        	int max = cnt;
+            for (int j = 0; j < n - 1; j++) {
+            	if (arr[i][j] == arr[i][j + 1]) {
+            		cnt++;
+            	} else {
+            		cnt = 1;
+            	}
+            	max = Math.max(cnt, max);
+            }
+            if (max >= m) {
+    			sum++;
+    		}
+        }
+        
+        for (int i = 0; i < n; i++) {
+        	int cnt = 1;
+        	int max = cnt;
+            for (int j = 0; j < n - 1; j++) {
+            	if (arr[j][i] == arr[j + 1][i]) {
+            		cnt++;
+            	} else {
+            		cnt = 1;
+            	}
+            	max = Math.max(cnt, max);
+            }
+            if (max >= m) {
+    			sum++;
+    		}
+        }
+        System.out.println(sum);
+
+    }
+}
