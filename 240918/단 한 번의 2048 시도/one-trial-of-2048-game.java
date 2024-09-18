e19c7d64b5fb9a7c0fcc24c4c43597b013075812
YoungEunLee <61973941+cnsejr2@users.noreply.github.com>
2024-09-18 22:26:52 +0900

    Title: 단 한 번의 2048 시도, Time: 105ms, Memory: 8MB, Status: Wrong Answer - CodeTree

--- 240918/단 한 번의 2048 시도/one-trial-of-2048-game.java
+++ 240918/단 한 번의 2048 시도/one-trial-of-2048-game.java
@@ -37,6 +37,15 @@ public class Main {
 	private static void rotate(String dir) {
 		// TODO Auto-generated method stub
 		if (dir.equals("L")) {
+			temp = new int[4][4];
+			for (int i = 0; i < 4; i++) {
+				int t = 0;
+				for (int j = 0; j < 4; j++) {
+					if (map[i][j] > 0) {
+						temp[i][t++] = map[i][j];
+					}
+				}
+			}
 			for (int i = 0; i < 4; i++) {
 				int prev = map[i][0];
 				for (int j = 1; j < 4; j++) {
@@ -59,6 +68,15 @@ public class Main {
 				}
 			}
 		} else if (dir.equals("R")) {
+			temp = new int[4][4];
+			for (int i = 0; i < 4; i++) {
+				int t = 3;
+				for (int j = 3; j >= 0; j--) {
+					if (map[i][j] > 0) {
+						temp[i][t--] = map[i][j];
+					}
+				}
+			}
 			for (int i = 3; i >= 0; i--) {
 				int prev = map[i][3];
 				for (int j = 2; j >= 0; j--) {
@@ -81,6 +99,15 @@ public class Main {
 				}
 			}
 		} else if (dir.equals("U")) {
+			temp = new int[4][4];
+			for (int i = 0; i < 4; i++) {
+				int t = 0;
+				for (int j = 0; j < 4; j++) {
+					if (map[j][i] > 0) {
+						temp[t++][i] = map[j][i];
+					}
+				}
+			}
 			for (int i = 0; i < 4; i++) {
 				int prev = map[0][i];
 				for (int j = 1; j < 4; j++) {
