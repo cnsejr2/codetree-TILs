94ebb7caa32357897538ab07fdb7a508168b572a
YoungEunLee <61973941+cnsejr2@users.noreply.github.com>
2024-09-22 16:59:36 +0900

    Title: 계단 오르기, Time: 134ms, Memory: 10MB, Status: Passed - CodeTree

--- 240922/계단 오르기/climbing-stairs.java
+++ 240922/계단 오르기/climbing-stairs.java
@@ -1,33 +1,32 @@
 import java.util.Scanner;
 
 public class Main {
-	
-	static int[] dp;
+    public static final int MOD = 10007;
+    public static final int MAX_N = 1000;
+    
+    // 변수 선언
+    public static int n;
+    
+    public static int[] dp = new int[MAX_N + 1];
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		Scanner sc = new Scanner(System.in);
-		
-		int n = sc.nextInt();
-		dp = new int[n + 1];
-		System.out.println(check(n - 1) % 10007);
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        // 입력:
+        n = sc.nextInt();
 
-		sc.close();
+        // 초기 조건 설정
+        dp[0] = 1;
+        dp[1] = 0;
+        dp[2] = 1;
+        dp[3] = 1;
 
-	}
+        // 점화식에 따라 dp값 채우기
+        // dp[i] = dp[i - 2] + dp[i - 3]
+        for(int i = 4; i <= n; i++)
+            dp[i] = (dp[i - 2] + dp[i - 3]) % MOD;
 
-	private static int check(int num) {
-		if (dp[num] != 0) {
-			return dp[num];
-		}
-		if (num == 0) {
-			return 0;
-		} else if (num <= 2) {
-			return 1;
-		}
-		dp[num] = check(num - 2) + check(num - 3);
-		return dp[num];
-		
-	}
-
+        System.out.print(dp[n]);
+        
+        sc.close();
+    }
 }
