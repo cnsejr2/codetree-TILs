9c5ed80cfb4062bdeeb141b88182598981600e78
YoungEunLee <61973941+cnsejr2@users.noreply.github.com>
2024-10-01 12:29:47 +0900

    Title: 4가지 연산을 이용하여 1 만들기, Time: 657ms, Memory: 61MB, Status: Passed - CodeTree

--- 241001/4가지 연산을 이용하여 1 만들기/make-one-using-four-operations.java
+++ 241001/4가지 연산을 이용하여 1 만들기/make-one-using-four-operations.java
@@ -1,68 +1,43 @@
-import java.util.ArrayDeque;
+import java.util.LinkedList;
 import java.util.Queue;
 import java.util.Scanner;
 
 public class Main {
 	
-	static int n;
-	static int[] arr = new int[1000001];
-	static int[] visit = new int[1000001];
-	
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		Scanner sc = new Scanner(System.in);
-		
-		n = sc.nextInt();
-		
-		bfs();
-		
-		System.out.println(visit[n]);
-		
-		sc.close();
+	public static int MAX = 1000000;
+    public static int n; //숫자
 
-	}
+    public static int[] result = new int[MAX + 1];
+    public static boolean[] visit = new boolean[MAX + 1];
+    public static Queue<Integer> q = new LinkedList<>();
+    
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+        int n = sc.nextInt();
 
-	private static void bfs() {
-		// TODO Auto-generated method stub
-		Queue<Integer> q = new ArrayDeque<>();
-		q.add(n);
-		
-		while (!q.isEmpty()) {
-			int cur = q.poll();
-			
-			for (int i = 1; i <= 4; i++) {
-				int next = cur;
-				if (i == 1) {
-					next--;
-				} else if (i == 2) {
-					next++;
-				} else if (i == 3) {
-					if (next % 2 == 0) {
-						next /= 2;
-					} else {
-						continue;
-					}
-				} else if (i == 4) {
-					if (next % 3 == 0) {
-						next /= 3;
-					} else {
-						continue;
-					}
-				}
-				if (next > 0) {
-					if (next == 1) {
-						return;
-					} else {
-						visit[next] = visit[cur] + 1;
-						q.add(next);
-					}
-				}
-				
-				
-			}
-		}
-		
-		
-	}
+        visit[1] = true;
+        q.add(1);
+
+        while (!q.isEmpty()) {
+            int cur = q.poll();
+            int[] op = { -1, 1, cur, cur * 2};
+            
+            for (int i = 0; i < 4; i++) {
+                int nx = cur + op[i];
+                if (nx >= 0 && nx <= MAX) {
+                	if (!visit[nx]) {
+                		visit[nx] = true;
+                		result[nx] = result[cur] + 1;
+                    	q.add(nx);
+                	}
+                }
+            }
+        }
+        
+        System.out.println(result[n]);
+        
+        sc.close();
+    }
 
 }
