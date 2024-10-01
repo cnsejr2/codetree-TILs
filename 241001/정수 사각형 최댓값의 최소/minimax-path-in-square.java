0cf7a09927d9b8e12dbf99a13f47b8b91f65f974
YoungEunLee <61973941+cnsejr2@users.noreply.github.com>
2024-10-01 21:50:23 +0900

    Title: 정수 사각형 최댓값의 최소, Time: 136ms, Memory: 11MB, Status: Passed - CodeTree

---
+++ 241001/정수 사각형 최댓값의 최소/minimax-path-in-square.java
@@ -0,0 +1,40 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+
+	public static void main(String[] args) throws IOException {
+		// TODO Auto-generated method stub
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+
+		int n = Integer.parseInt(st.nextToken());
+		int[][] arr = new int[n + 1][n + 1];
+		
+		for (int i = 1; i <= n; i++) {
+			st = new StringTokenizer(br.readLine());
+			for (int j = 1; j <= n; j++) {
+				arr[i][j] = Integer.parseInt(st.nextToken());
+			}
+		}
+		
+		int[][] dp = new int[n + 1][n + 1];
+		dp[1][1] = arr[1][1];
+		
+		for (int i = 2; i <= n; i++) {
+			dp[1][i] = Math.max(arr[1][i], dp[1][i - 1]);
+			dp[i][1] = Math.max(arr[i][1], dp[i - 1][1]);
+		}
+		
+		for (int i = 2; i <= n; i++) {
+			for (int j = 2; j <= n; j++) {
+				dp[i][j] = Math.max(Math.min(dp[i - 1][j], dp[i][j - 1]), arr[i][j]);
+			}
+		}
+		
+		System.out.println(dp[n][n]);
+	}
+
+}
