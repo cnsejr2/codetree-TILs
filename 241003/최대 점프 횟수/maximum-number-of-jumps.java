5025620708559855f24761e0e0acb6ccf3743e2b
YoungEunLee <61973941+cnsejr2@users.noreply.github.com>
2024-10-03 15:55:19 +0900

    Title: 최대 점프 횟수, Time: 184ms, Memory: 13MB, Status: Passed - CodeTree

--- 241003/최대 점프 횟수/maximum-number-of-jumps.java
+++ 241003/최대 점프 횟수/maximum-number-of-jumps.java
@@ -9,12 +9,13 @@ public class Main {
 		int n = sc.nextInt();
 		int[] arr = new int[n + 1];
 		
-		for (int i = 1; i <= n; i++) {
+		for (int i = 0; i < n; i++) {
 			arr[i] = sc.nextInt();
 		}
-		int[] dp = new int[n + 1];
-		Arrays.fill(dp, Integer.MIN_VALUE);
-		dp[0] = 0;
+		 int[] dp = new int[n + 1];   
+		 Arrays.fill(dp, Integer.MIN_VALUE); 
+		 dp[0] = 0;
+	 
 		
 		for (int i = 1; i < n; i++) {
 			for (int j = 0; j < i; j++) {
