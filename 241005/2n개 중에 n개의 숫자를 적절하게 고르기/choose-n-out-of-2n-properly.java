e6a97f9639555e0c6ad0735092f4401a8236d855
YoungEunLee <61973941+cnsejr2@users.noreply.github.com>
2024-10-05 03:04:10 +0900

    Title: 2n개 중에 n개의 숫자를 적절하게 고르기, Time: 112ms, Memory: 10MB, Status: Passed - CodeTree

--- 241005/2n개 중에 n개의 숫자를 적절하게 고르기/choose-n-out-of-2n-properly.java
+++ 241005/2n개 중에 n개의 숫자를 적절하게 고르기/choose-n-out-of-2n-properly.java
@@ -1,12 +1,9 @@
-import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Main {
 	
 	static int n;
 	static int[] arr;
-	static boolean[] visit;
-	static ArrayList<Integer> list = new ArrayList<>();
 	static int answer = Integer.MAX_VALUE;
 	static int total;
 
@@ -16,41 +13,33 @@ public class Main {
 		
 		n = sc.nextInt();
 		arr = new int[2 * n];
-		visit = new boolean[2 * n];
 		total = 0;
 		for (int i = 0; i < 2 * n; i++) {
 			arr[i] = sc.nextInt();
 			total += arr[i];
 		}
 		
-		comb(0, 0);
+		comb(0, 0, 0);
 		
 		System.out.println(answer);
 		
 		sc.close();
 	}
 
-	private static void comb(int depth, int s) {
+	private static void comb(int s, int cnt, int sum) {
 		// TODO Auto-generated method stub
-		if (depth == n) {
-			int sum = 0;
-			for (int i = 0; i < list.size(); i++) {
-					sum += list.get(i);
-			}
+		if (cnt == n) {
 			int num = total - sum;
-			answer = Math.min(answer, Math.abs(num - sum));
+			answer = Math.min(answer, Math.abs(sum - num));
 			return;
 		}
 		
-		for (int i = s; i < 2 * n; i++) {
-			if (!visit[i]) {
-				visit[i] = true;
-				list.add(arr[i]);
-				comb(depth + 1, s + 1);
-				list.remove(list.size() - 1);
-				visit[i] = false;
-			}
+		if (n * 2 == s) {
+			return;
 		}
+		
+		comb(s + 1, cnt, sum);
+		comb(s + 1, cnt + 1, sum + arr[s]);
 		
 	}
 
