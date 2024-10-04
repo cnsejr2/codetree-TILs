b5bcf9150e3da7278423e1e2ed64f2cb6a15da0b
YoungEunLee <61973941+cnsejr2@users.noreply.github.com>
2024-10-05 02:01:02 +0900

    Title: 단순한 동전 챙기기, Time: 291ms, Memory: 18MB, Status: Passed - CodeTree

--- 241005/단순한 동전 챙기기/collect-coins-easy.java
+++ 241005/단순한 동전 챙기기/collect-coins-easy.java
@@ -3,72 +3,80 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.Collections;
-import java.util.Comparator;
 import java.util.LinkedList;
 import java.util.Queue;
-import java.util.StringTokenizer;
 
 public class Main {
 	
+	 public static class Coin {
+    	int num;
+        int x;
+        int y;
+        public Coin(int num, int x, int y){
+        	this.num = num;
+            this.x = x;
+            this.y = y;
+        }
+    } 
+	
 	public static int n;
     public static boolean [][] visited;
-    public static int [][] dist;
+    public static int [][] map;
     public static boolean [] used;
-    public static ArrayList<Tuple> coins = new ArrayList<>();
-    public static ArrayList<Tuple> selected = new ArrayList<>();
+    public static ArrayList<Coin> coins = new ArrayList<>();
+    public static ArrayList<Coin> selected = new ArrayList<>();
     public static int [] dx = {0, 0, -1, 1};
     public static int [] dy = {-1, 1, 0 ,0};
-    public static Tuple s;
-    public static Tuple e;
-    public static int ans = Integer.MAX_VALUE;;
+    public static Coin start;
+    public static Coin end;
+    public static int answer = Integer.MAX_VALUE;;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         n = Integer.parseInt(br.readLine());
 
-        for(int i = 0 ; i < n ; i++){
+        for(int i = 1 ; i <= n ; i++){
             String str = br.readLine();
-            for(int j = 0 ; j < n ; j++){
-                int c = str.charAt(j);
+            for(int j = 1 ; j <= n ; j++){
+                int c = str.charAt(j - 1);
                 if(c == 'S'){
-                    s = new Tuple(j, i, 0);
+                    start = new Coin(0, i, j);
                 }
                 else if(c == 'E') {
-                    e = new Tuple(j, i, 0);
+                    end = new Coin(0, i, j);
                 }
                 else if(c != '.') {
-                    coins.add(new Tuple(j, i, c-'0'));
+                    coins.add(new Coin(c - '0', i, j));
                 }
             }
         }
-
+        if (coins.size() < 3) {
+        	System.out.println(-1);
+        	return;
+        }
         used = new boolean[coins.size()];
-        Collections.sort(coins, (Tuple a, Tuple b) -> { // 코인 숫자 오름차순으로 정렬하자
+        Collections.sort(coins, (Coin a, Coin b) -> { // 코인 숫자 오름차순으로 정렬하자
             return a.num - b.num;
         });
 
         dfs(0, 0);
-        if(ans == Integer.MAX_VALUE){
-            System.out.println(-1);
-        } else {
-            System.out.println(ans);
-        }
+        System.out.println(answer);
     }
     
-    public static void dfs(int depth, int start){
+    public static void dfs(int depth, int s){
         if(depth >= 3){
             int total=0;
             for(int i = 0 ; i < selected.size() - 1; i++){
                 total += getDistance(selected.get(i), selected.get(i+1));
             }
-            total += (getDistance(s, selected.get(0)) + getDistance(selected.get(selected.size() - 1), e));
+            total += (getDistance(start, selected.get(0)) + getDistance(selected.get(selected.size() - 1), end));
 
-            ans = Math.min(total, ans);
+            answer = Math.min(total, answer);
 
             return;
         }
         // 가능한 모든 조합을 찾자
-        for(int i = start ; i < coins.size(); i++){
+        for(int i = s ; i < coins.size(); i++){
             if(!used[i]){
                 used[i] = true;
                 selected.add(coins.get(i));
@@ -79,42 +87,34 @@ public class Main {
         }
     }
 
-    public static int getDistance(Tuple from, Tuple to){
+    public static int getDistance(Coin from, Coin to){
         // from -> to 까지의 최단 거리를 구하자
-        dist = new int[n][n];
-        visited = new boolean[n][n];
+        map = new int[n + 1][n + 1];
+        visited = new boolean[n + 1][n + 1];
 
-        Queue<Tuple> q = new LinkedList<>();
+        Queue<Coin> q = new LinkedList<>();
         q.add(from);
-        visited[from.y][from.x] = true;
-        // bfs
+        visited[from.x][from.y] = true;
+
         while(!q.isEmpty()){
-            Tuple now = q.poll();
+            Coin now = q.poll();
             if(now.x == to.x && now.y == to.y){
                 break;
             }
             for(int dir = 0 ; dir < 4 ; dir ++){
                 int nx = now.x + dx[dir];
                 int ny = now.y + dy[dir];
-                if(nx < 0 || nx >= n || ny < 0 || ny >= n) continue;
-                if(visited[ny][nx]) continue;
-                dist[ny][nx] = dist[now.y][now.x] + 1;
-                q.offer(new Tuple(nx, ny, 0));
-                visited[ny][nx] = true;
+                if(nx > 0 && nx <= n && ny > 0 && ny <= n) {
+                	if(!visited[nx][ny]) {
+		                map[nx][ny] = map[now.x][now.y] + 1;
+		                q.add(new Coin(0, nx, ny));
+		                visited[nx][ny] = true;
+                	}
+                }
             }
         }
-        return dist[to.y][to.x];
+        return map[to.x][to.y];
     }
 
-    public static class Tuple {
-        int x;
-        int y;
-        int num;
-        public Tuple(int x, int y, int num){
-            this.x = x;
-            this.y = y;
-            this.num = num;
-        }
-    } 
 
 }
