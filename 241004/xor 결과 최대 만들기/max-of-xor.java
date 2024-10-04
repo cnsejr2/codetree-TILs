0a43057a624986f20ee5bf811fbbe43b0dad12df
YoungEunLee <61973941+cnsejr2@users.noreply.github.com>
2024-10-04 20:47:36 +0900

    Title: xor 결과 최대 만들기, Time: 120ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 241004/xor 결과 최대 만들기/max-of-xor.java
@@ -0,0 +1,52 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Main {
+	
+	static int n, m;
+	static int[] arr;
+	static ArrayList<Integer> list = new ArrayList<>();
+	static int max = Integer.MIN_VALUE;
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		
+		n = sc.nextInt();
+		m = sc.nextInt();
+		
+		arr = new int[n + 1];
+		for (int i = 1; i <= n; i++) {
+			arr[i] = sc.nextInt();
+		}
+		Arrays.sort(arr);
+		
+		comb(1, 0);
+		
+		System.out.println(max);
+				
+		sc.close();
+
+	}
+
+	private static void comb(int lastNum, int depth) {
+		// TODO Auto-generated method stub
+		if (depth == m) {
+			int result = list.get(0);
+			for (int i = 1; i < list.size(); i++) {
+				result = result ^ list.get(i);
+			}
+			max = Math.max(max, result);
+			return;
+		}
+		
+		for (int i = lastNum; i <= n; i++) {
+			list.add(i);
+			comb(i + 1, depth + 1);
+			list.remove(list.size() - 1);
+		}
+		
+	}
+
+}
