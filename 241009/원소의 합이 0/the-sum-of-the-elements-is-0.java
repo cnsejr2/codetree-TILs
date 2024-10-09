8fe07db92f2137a5e0b186c81a1c4ab4756c5b81
YoungEunLee <61973941+cnsejr2@users.noreply.github.com>
2024-10-09 19:42:31 +0900

    Title: 원소의 합이 0, Time: 122ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 241009/원소의 합이 0/the-sum-of-the-elements-is-0.java
@@ -0,0 +1,50 @@
+import java.util.HashMap;
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		
+		int n = sc.nextInt();
+		HashMap<Integer, Long> a = new HashMap<>();
+		HashMap<Integer, Long> b = new HashMap<>();
+		HashMap<Integer, Long> c = new HashMap<>();
+		HashMap<Integer, Long> d = new HashMap<>();
+		
+		for (int i = 0; i < n; i++) {
+			Long num = sc.nextLong();
+			a.put(i, num);
+		}
+		for (int i = 0; i < n; i++) {
+			Long num = sc.nextLong();
+			b.put(i, num);
+		}
+		for (int i = 0; i < n; i++) {
+			Long num = sc.nextLong();
+			c.put(i, num);
+		}
+		for (int i = 0; i < n; i++) {
+			Long num = sc.nextLong();
+			d.put(i, num);
+		}
+		int cnt = 0;
+		for (int i = 0; i < n; i++) {
+			for (int j = 0; j < n; j++) {
+				for (int k = 0; k < n; k++) {
+					long sum = a.get(i) + b.get(j) + c.get(k);
+					if (d.containsValue(sum * -1)) {
+						cnt++;
+					}
+				}
+			}
+		}
+		System.out.println(cnt);
+		
+		
+		sc.close();
+
+	}
+
+}
