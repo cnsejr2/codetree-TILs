69663db87ca3280d51f2dbeae9a2f52d77b98f4a
YoungEunLee <61973941+cnsejr2@users.noreply.github.com>
2024-10-09 15:39:32 +0900

    Title: 대응되는 수와 문자, Time: 2298ms, Memory: 65MB, Status: Passed - CodeTree

--- 241009/대응되는 수와 문자/corresponding-numbers-and-characters.java
+++ 241009/대응되는 수와 문자/corresponding-numbers-and-characters.java
@@ -1,40 +1,35 @@
 import java.util.HashMap;
-import java.util.Map;
 import java.util.Scanner;
 
 public class Main {
+    
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        // 입력:
+        int n = sc.nextInt();
+        int m = sc.nextInt();
+        
+        String[] words = new String[n + 1];
+        
+        HashMap<String, Integer> StringToNum = new HashMap<>();
+        for(int i = 1; i <= n; i++) {
+            words[i] = sc.next();
+            StringToNum.put(words[i], i);
+        }
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		Scanner sc = new Scanner(System.in);
-		
-		int n = sc.nextInt();
-		int m = sc.nextInt();
-		
-		HashMap<String, String> map = new HashMap<>();
-		HashMap<String, String> reverseMap = new HashMap<>(); // 값을 기준으로도 찾을 수 있게 하기 위해 역으로 저장
-		
-		for (int i = 1; i <= n; i++) {
-		    String str = sc.next();
-		    map.put(str, String.valueOf(i));
-		    reverseMap.put(String.valueOf(i), str);
-		}
-		
-		for (int i = 0; i < m; i++) {
-		    String query = sc.next();
-		    
-		    // 키로 검색
-		    if (map.containsKey(query)) {
-		        System.out.println(map.get(query));
-		    } 
-		    // 값으로 검색
-		    else if (reverseMap.containsKey(query)) {
-		        System.out.println(reverseMap.get(query));
-		    }
-		}
-		
-		sc.close();
+        while(m-- > 0) {
+            String key = sc.next();
 
-	}
-
+            // 입력된 값이 숫자일 때에는 array에 저장한 문자를 출력합니다.
+            if('0' <= key.charAt(0) && key.charAt(0) <= '9')
+                System.out.println(words[Integer.parseInt(key)]);
+            
+            // 입력된 값이 문자일 때에는 hashmap에 기록된 대응되는 숫자를 출력합니다.
+            else
+                System.out.println(StringToNum.get(key));
+        }
+        
+        
+        sc.close();
+    }
 }
