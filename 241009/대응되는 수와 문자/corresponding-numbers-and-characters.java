03d9bca416fa426ea863db2c467ddb6867a0d618
YoungEunLee <61973941+cnsejr2@users.noreply.github.com>
2024-10-09 15:21:34 +0900

    Title: 대응되는 수와 문자, Time: 1915ms, Memory: 61MB, Status: Passed - CodeTree

--- 241009/대응되는 수와 문자/corresponding-numbers-and-characters.java
+++ 241009/대응되는 수와 문자/corresponding-numbers-and-characters.java
@@ -6,38 +6,34 @@ public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-Scanner sc = new Scanner(System.in);
+		Scanner sc = new Scanner(System.in);
 		
 		int n = sc.nextInt();
 		int m = sc.nextInt();
 		
 		HashMap<String, String> map = new HashMap<>();
+		HashMap<String, String> reverseMap = new HashMap<>(); // 값을 기준으로도 찾을 수 있게 하기 위해 역으로 저장
+		
 		for (int i = 1; i <= n; i++) {
-			String str = sc.next();
-			
-			map.put(str, String.valueOf(i));
+		    String str = sc.next();
+		    map.put(str, String.valueOf(i));
+		    reverseMap.put(String.valueOf(i), str);
 		}
 		
 		for (int i = 0; i < m; i++) {
-			String str = sc.next();
-            for (Map.Entry<String, String> entry : map.entrySet()) {
-                String key = entry.getKey();
-                String value = entry.getValue();
-                
-                if (key.equals(str)) {
-                    System.out.println(value);
-                    break;
-                } else if (value.equals(str)) {
-                    System.out.println(key);
-                    break;
-                }
-          
-            }
-        
+		    String query = sc.next();
+		    
+		    // 키로 검색
+		    if (map.containsKey(query)) {
+		        System.out.println(map.get(query));
+		    } 
+		    // 값으로 검색
+		    else if (reverseMap.containsKey(query)) {
+		        System.out.println(reverseMap.get(query));
+		    }
 		}
 		
 		sc.close();
-		
 
 	}
 
