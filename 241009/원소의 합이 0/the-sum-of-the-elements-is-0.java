579f36897b1aece4a7ad1c1803e5bc36bc209a61
YoungEunLee <61973941+cnsejr2@users.noreply.github.com>
2024-10-09 19:49:30 +0900

    Title: 원소의 합이 0, Time: 678ms, Memory: 38MB, Status: Passed - CodeTree

--- 241009/원소의 합이 0/the-sum-of-the-elements-is-0.java
+++ 241009/원소의 합이 0/the-sum-of-the-elements-is-0.java
@@ -6,46 +6,51 @@ public class Main {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Scanner sc = new Scanner(System.in);
-		
-		int n = sc.nextInt();
-		HashMap<Integer, Long> a = new HashMap<>();
-		HashMap<Integer, Long> b = new HashMap<>();
-		HashMap<Integer, Long> c = new HashMap<>();
-		HashMap<Integer, Long> d = new HashMap<>();
-		
-		for (int i = 0; i < n; i++) {
-			Long num = sc.nextLong();
-			a.put(i, num);
-		}
-		for (int i = 0; i < n; i++) {
-			Long num = sc.nextLong();
-			b.put(i, num);
-		}
-		for (int i = 0; i < n; i++) {
-			Long num = sc.nextLong();
-			c.put(i, num);
-		}
-		for (int i = 0; i < n; i++) {
-			Long num = sc.nextLong();
-			d.put(i, num);
-		}
-		int cnt = 0;
-		for (int i = 0; i < n; i++) {
-			for (int j = 0; j < n; j++) {
-				for (int k = 0; k < n; k++) {
-					for (int l = 0; l < n; l++) {
-						long sum = a.get(i) + b.get(j) + c.get(k) + d.get(l);
-						if (sum == 0) {
-							cnt++;
-						}
-					}
-				}
-			}
-		}
-		System.out.println(cnt);
-		
-		
-		sc.close();
+        
+        int n = sc.nextInt();
+        long[] a = new long[n];
+        long[] b = new long[n];
+        long[] c = new long[n];
+        long[] d = new long[n];
+        
+        for (int i = 0; i < n; i++) {
+            a[i] = sc.nextLong();
+        }
+        for (int i = 0; i < n; i++) {
+            b[i] = sc.nextLong();
+        }
+        for (int i = 0; i < n; i++) {
+            c[i] = sc.nextLong();
+        }
+        for (int i = 0; i < n; i++) {
+            d[i] = sc.nextLong();
+        }
+        
+        // a[i] + b[j]의 합을 저장할 해시맵
+        HashMap<Long, Integer> abSum = new HashMap<>();
+        
+        // a와 b의 모든 합을 해시맵에 저장
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                long sumAB = a[i] + b[j];
+                abSum.put(sumAB, abSum.getOrDefault(sumAB, 0) + 1);
+            }
+        }
+        
+        int cnt = 0;
+        
+        // c와 d의 모든 합의 음수를 해시맵에서 찾음
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                long sumCD = -(c[i] + d[j]);
+                if (abSum.containsKey(sumCD)) {
+                    cnt += abSum.get(sumCD); // 가능한 경우의 수만큼 더함
+                }
+            }
+        }
+        
+        System.out.println(cnt);
+        sc.close();
 
 	}
 
