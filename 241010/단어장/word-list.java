8158e891def3cade4f279f00fe1e6c034ff0e1bc
YoungEunLee <61973941+cnsejr2@users.noreply.github.com>
2024-10-10 20:38:11 +0900

    Title: 단어장, Time: 1252ms, Memory: 30MB, Status: Passed - CodeTree

---
+++ 241010/단어장/word-list.java
@@ -0,0 +1,35 @@
+import java.util.Iterator;
+import java.util.Scanner;
+import java.util.TreeMap;
+import java.util.Map.Entry;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+        Scanner sc = new Scanner(System.in);
+		
+		int n = sc.nextInt();
+		TreeMap<String, Integer> map = new TreeMap<>();
+		
+		for (int i = 0; i < n; i++) {
+			String s = sc.next();
+			if (map.containsKey(s)) {
+				map.put(s, map.get(s) + 1);
+			} else {
+				map.put(s, 1);
+			}
+		}
+		Iterator<Entry<String, Integer>> it = map.entrySet().iterator();
+
+		while (it.hasNext()) {
+			Entry<String, Integer> entry = it.next();
+			
+			System.out.println(entry.getKey() + " " + entry.getValue());
+		}
+		System.out.println();
+
+		sc.close();
+	}
+
+}
