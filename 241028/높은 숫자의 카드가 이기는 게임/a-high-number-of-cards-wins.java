658b53aa289d751c7bfab9d76a7b1ab9a538f0e4
YoungEunLee <61973941+cnsejr2@users.noreply.github.com>
2024-10-28 19:44:39 +0900

    Title: 높은 숫자의 카드가 이기는 게임, Time: 766ms, Memory: 33MB, Status: Passed - CodeTree

--- 241028/높은 숫자의 카드가 이기는 게임/a-high-number-of-cards-wins.java
+++ 241028/높은 숫자의 카드가 이기는 게임/a-high-number-of-cards-wins.java
@@ -1,34 +1,53 @@
-import java.util.Arrays;
-import java.util.Scanner;
+import java.util.*;
 
 public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+        int N = sc.nextInt();
+        int[] B_cards = new int[N];
+        
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		Scanner sc = new Scanner(System.in);
-		
-		int n = sc.nextInt();
-		
-		boolean[] visit = new boolean[n * 2 + 1];
-		int[] arr = new int[n * 2 + 1];
-		for (int i = 0; i < n; i++) {
-			arr[i] = sc.nextInt();
-			visit[arr[i]] = true;
-		}
-		int cnt = 0;
-		for (int i = 0; i < n; i++) {
-			for (int j = arr[i] + 1; j < n * 2 + 1; j++) {
-				if (!visit[j]) {
-					visit[j] = true;
-					cnt++;
-					break;
-				}
-			}
-		}
-		System.out.println(cnt);
-		
-		sc.close();
-
-	}
-
+        for (int i = 0; i < N; i++) {
+            B_cards[i] = sc.nextInt();
+        }
+        
+        // 전체 카드 리스트 만들기
+        List<Integer> all_cards = new ArrayList<>();
+        for (int i = 1; i <= 2 * N; i++) {
+            all_cards.add(i);
+        }
+        
+        // A_cards 리스트 만들기 (B_cards에 없는 카드들만 추가)
+        Set<Integer> b_set = new HashSet<>();
+        for (int b : B_cards) {
+            b_set.add(b);
+        }
+        
+        List<Integer> A_cards = new ArrayList<>();
+        for (int card : all_cards) {
+            if (!b_set.contains(card)) {
+                A_cards.add(card);
+            }
+        }
+        
+        // A_cards와 B_cards 정렬
+        Collections.sort(A_cards);
+        Arrays.sort(B_cards);
+        
+        int ans = 0;
+        int j = 0;
+        
+        // 이길 수 있는 경우의 수 계산
+        for (int i = 0; i < N; i++) {
+            if (A_cards.get(i) > B_cards[j] && j < N) {
+                ans++;
+                j++;
+            }
+        }
+        
+        System.out.println(ans);
+        
+        sc.close();
+    }
 }
