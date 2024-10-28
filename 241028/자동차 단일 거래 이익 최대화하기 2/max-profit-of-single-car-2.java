ab772571333943796abe790dadb8823d25a7df53
YoungEunLee <61973941+cnsejr2@users.noreply.github.com>
2024-10-28 19:01:50 +0900

    Title: 자동차 단일 거래 이익 최대화하기 2, Time: 811ms, Memory: 35MB, Status: Passed - CodeTree

--- 241028/자동차 단일 거래 이익 최대화하기 2/max-profit-of-single-car-2.java
+++ 241028/자동차 단일 거래 이익 최대화하기 2/max-profit-of-single-car-2.java
@@ -8,18 +8,15 @@ public class Main {
 		
 		int n = sc.nextInt();
 		long[] arr = new long[n + 1];
+		long[] dp = new long[n + 1];
+		dp[0] = Long.MAX_VALUE;
 		for (int i = 1; i <= n; i++) {
 			arr[i] = sc.nextLong();
+			dp[i] = Math.min(dp[i - 1], arr[i]);
 		}
 		long profit = 0;
 		for (int i = n; i > 0; i--) {
-			long cur = arr[i];
-			long min = Integer.MAX_VALUE;
-			for (int j = i - 1; j > 0; j--) {
-				min = Math.min(min, arr[j]);
-			}
-			
-			profit = Math.max(profit, cur - min);
+			profit = Math.max(profit, arr[i] - dp[i]);
 		}
 		System.out.println(profit);
 		
