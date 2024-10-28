eb327a405528d6490a6270f5dea87ddd54c55457
YoungEunLee <61973941+cnsejr2@users.noreply.github.com>
2024-10-28 20:42:35 +0900

    Title: 폭탄 해체 작업, Time: 708ms, Memory: 25MB, Status: Passed - CodeTree

--- 241028/폭탄 해체 작업/the-bomb-dismantling.java
+++ 241028/폭탄 해체 작업/the-bomb-dismantling.java
@@ -1,31 +1,45 @@
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Scanner;
+import java.util.*;
 
 public class Main {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		Scanner sc = new Scanner(System.in);
-		int n = sc.nextInt();
-		HashMap<Integer, Integer> map = new HashMap<Integer, Integer>();
-		
-		for (int i = 0; i < n; i++) {
-			int score = sc.nextInt();
-			int time = sc.nextInt();
-			int sMax = Math.max(score, map.getOrDefault(time, 0));
-			map.put(time, sMax);
-
-		}
-		int sum = 0;
-		for ( Map.Entry<Integer, Integer> entry : map.entrySet()) {
-			sum += entry.getValue();
-		}
-		System.out.println(sum);
-		
-		
-		sc.close();
-
-	}
-
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        
+        List<int[]> bombs = new ArrayList<>();
+        for (int i = 0; i < t; i++) {
+            int score = sc.nextInt();
+            int limit = sc.nextInt();
+            bombs.add(new int[]{score, limit});
+        }
+        
+        // limit 기준, 그 후 score 기준으로 정렬
+        bombs.sort((a, b) -> a[1] == b[1] ? b[0] - a[0] : a[1] - b[1]);
+        int MAX_TIME = bombs.get(bombs.size() - 1)[1];
+        
+        List<List<Integer>> bombList = new ArrayList<>();
+        for (int i = 0; i <= MAX_TIME; i++) {
+            bombList.add(new ArrayList<>());
+        }
+        
+        for (int[] bomb : bombs) {
+            int score = bomb[0];
+            int limit = bomb[1];
+            bombList.get(limit).add(score);
+        }
+        
+        PriorityQueue<Integer> heap = new PriorityQueue<>(Collections.reverseOrder());
+        int cnt = 0;
+        
+        for (int time = MAX_TIME; time > 0; time--) {
+            for (int score : bombList.get(time)) {
+                heap.add(score);
+            }
+            if (!heap.isEmpty()) {
+                cnt += heap.poll();
+            }
+        }
+        
+        System.out.println(cnt);
+        sc.close();
+    }
 }
