e7382b3323166386dbd2e24b1749e2d53017d47e
YoungEunLee <61973941+cnsejr2@users.noreply.github.com>
2024-10-28 21:09:59 +0900

    Title: 폭탄 해체 작업, Time: 520ms, Memory: 23MB, Status: Passed - CodeTree

--- 241028/폭탄 해체 작업/the-bomb-dismantling.java
+++ 241028/폭탄 해체 작업/the-bomb-dismantling.java
@@ -1,45 +1,67 @@
-import java.util.*;
+import java.util.Arrays;
+import java.util.PriorityQueue;
+import java.util.Scanner;
 
 public class Main {
+	
+	static class Pair implements Comparable<Pair> {
+		int timeLimit, score;
+		
+		public Pair(int timeLimit, int score) {
+			this.timeLimit = timeLimit;
+			this.score = score;
+		}
+		
+		@Override
+		public int compareTo(Pair b) {
+			if (timeLimit != b.timeLimit) {
+				return timeLimit - b.timeLimit;
+			}
+			return score - b.score;
+		}
+		
+	}
+	
+	public static final int MAX_T = 10000;
+	public static final int MAX_N = 10000;
+	
+	public static int n;
+	public static Pair[] bombs = new Pair[MAX_N];
+	
+	public static int getTimeLimit(int index) {
+		return bombs[index].timeLimit;
+	}
+	
+	public static int getScore(int index) {
+		return bombs[index].score;
+	}
+	
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int t = sc.nextInt();
         
-        List<int[]> bombs = new ArrayList<>();
-        for (int i = 0; i < t; i++) {
-            int score = sc.nextInt();
-            int limit = sc.nextInt();
-            bombs.add(new int[]{score, limit});
+        n = sc.nextInt();
+        
+        for (int i = 0; i < n; i++) {
+        	int score = sc.nextInt();
+        	int timeLimit = sc.nextInt();
+        	
+        	bombs[i] = new Pair(timeLimit, score);
         }
+        Arrays.sort(bombs, 0, n);
+        PriorityQueue<Integer> pq = new PriorityQueue<>();
+        int index = n - 1;
+        int ans = 0;
         
-        // limit 기준, 그 후 score 기준으로 정렬
-        bombs.sort((a, b) -> a[1] == b[1] ? b[0] - a[0] : a[1] - b[1]);
-        int MAX_TIME = bombs.get(bombs.size() - 1)[1];
-        
-        List<List<Integer>> bombList = new ArrayList<>();
-        for (int i = 0; i <= MAX_TIME; i++) {
-            bombList.add(new ArrayList<>());
+        for (int t = MAX_T; t >= 1; t--) {
+        	while (index >= 0 && getTimeLimit(index) >= t) {
+        		pq.add(-getScore(index));
+        		index--;
+        	}
+        	
+        	if (!pq.isEmpty()) 
+        		ans += -pq.poll();
         }
-        
-        for (int[] bomb : bombs) {
-            int score = bomb[0];
-            int limit = bomb[1];
-            bombList.get(limit).add(score);
-        }
-        
-        PriorityQueue<Integer> heap = new PriorityQueue<>(Collections.reverseOrder());
-        int cnt = 0;
-        
-        for (int time = MAX_TIME; time > 0; time--) {
-            for (int score : bombList.get(time)) {
-                heap.add(score);
-            }
-            if (!heap.isEmpty()) {
-                cnt += heap.poll();
-            }
-        }
-        
-        System.out.println(cnt);
+        System.out.println(ans);
         sc.close();
     }
 }
