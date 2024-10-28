aebd94f6751c4157f0894b1cd58b1f7e013bde03
YoungEunLee <61973941+cnsejr2@users.noreply.github.com>
2024-10-28 19:07:17 +0900

    Title: 자동차 단일 거래 이익 최대화하기 2, Time: 741ms, Memory: 34MB, Status: Passed - CodeTree

--- 241028/자동차 단일 거래 이익 최대화하기 2/max-profit-of-single-car-2.java
+++ 241028/자동차 단일 거래 이익 최대화하기 2/max-profit-of-single-car-2.java
@@ -1,27 +1,36 @@
 import java.util.Scanner;
 
 public class Main {
+    public static final int MAX_NUM = 100000;
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		Scanner sc = new Scanner(System.in);
-		
-		int n = sc.nextInt();
-		long[] arr = new long[n + 1];
-		long[] dp = new long[n + 1];
-		dp[0] = Long.MAX_VALUE;
-		for (int i = 1; i <= n; i++) {
-			arr[i] = sc.nextLong();
-			dp[i] = Math.min(dp[i - 1], arr[i]);
-		}
-		long profit = 0;
-		for (int i = n; i > 0; i--) {
-			profit = Math.max(profit, arr[i] - dp[i]);
-		}
-		System.out.println(profit);
-		
-		sc.close();
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        // 변수 선언:
+        int n;
+        int[] price = new int[MAX_NUM];
 
-	}
+        // 입력:
+        n = sc.nextInt();
+        for(int i = 0; i < n; i++)
+            price[i] = sc.nextInt();
 
+        // 배열을 앞에서부터 순회하며 최소값을 갱신해줍니다.
+        // 각 원소에 대하여 해당 시점의 최소값과의 차이가
+        // 최대가 될 때를 갱신해줍니다.
+        int maxProfit = 0;
+        int minPrice = price[0];
+        for(int i = 0; i < n; i++) {
+            int profit = price[i] - minPrice;
+            
+            // 답을 갱신해줍니다.
+            if(profit > maxProfit)
+                maxProfit = profit;
+            
+            // 지금까지의 최솟값을 갱신해줍니다.
+            if(minPrice > price[i])
+                minPrice = price[i];
+        }
+
+        System.out.print(maxProfit);
+    }
 }
