f6a08b6b3f7f36558e8ce415cc261dacb2538d6b
YoungEunLee <61973941+cnsejr2@users.noreply.github.com>
2024-10-30 02:05:02 +0900

    Title: 앞에서부터 삭제하기 2, Time: 3032ms, Memory: 38MB, Status: Time Limit Exceed - CodeTree

--- 241030/앞에서부터 삭제하기 2/delete-it-from-the-beginning-2.java
+++ 241030/앞에서부터 삭제하기 2/delete-it-from-the-beginning-2.java
@@ -7,24 +7,22 @@ public class Main {
 		// TODO Auto-generated method stub
 		Scanner sc = new Scanner(System.in);
 		int n = sc.nextInt();
-		
+		PriorityQueue<Integer> pq = new PriorityQueue<>();
 		int[] arr = new int[n];
+		double sum = 0;
 		for (int i = 0; i < n; i++) {
 			arr[i] = sc.nextInt();
+			pq.add(arr[i]);
+			sum += arr[i];
 		}
 		double max = Double.MIN_VALUE;
 		for (int i = 0; i < n - 2; i++) {
-			PriorityQueue<Integer> pq = new PriorityQueue<>();
-			for (int j = i; j < n; j++) {
-				pq.add(arr[j]);
-			}
-			pq.poll();
-			int sum = 0;
-			double cnt = pq.size();
-			while (!pq.isEmpty()) {
-				sum += pq.poll();
-			}
-			max = Math.max(max, sum / cnt);
+			sum -= arr[i];
+			pq.remove(arr[i]);
+			int min = pq.peek();
+			sum -= min;
+			max = Math.max(max, sum / (pq.size() - 1));
+			sum += min;
 		}
 		System.out.printf("%.2f", max);
 		
