ca98ddc153f12153e2ce094e09a4a322d3e586a6
YoungEunLee <61973941+cnsejr2@users.noreply.github.com>
2024-10-29 21:01:25 +0900

    Title: 배열 추출, Time: 1772ms, Memory: 60MB, Status: Passed - CodeTree

---
+++ 241029/배열 추출/array-extraction.java
@@ -0,0 +1,30 @@
+import java.util.PriorityQueue;
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		
+		int n = sc.nextInt();
+		PriorityQueue<Integer> pq = new PriorityQueue<>();
+		for (int i = 0; i < n; i++) {
+			int num = sc.nextInt();
+			
+			if (num == 0) {
+				if (pq.isEmpty()) {
+					System.out.println(0);
+				} else {
+					System.out.println(-pq.poll());	
+				}
+			} else {
+				pq.add(-num);
+			}
+		}
+
+		sc.close();
+
+	}
+
+}
