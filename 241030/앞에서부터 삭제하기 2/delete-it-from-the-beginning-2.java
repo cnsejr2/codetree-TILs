3602c27ff5707c3e9b4d32c70f2181876a85df91
YoungEunLee <61973941+cnsejr2@users.noreply.github.com>
2024-10-30 02:50:42 +0900

    Title: 앞에서부터 삭제하기 2, Time: 131ms, Memory: 10MB, Status: Wrong Answer - CodeTree

--- 241030/앞에서부터 삭제하기 2/delete-it-from-the-beginning-2.java
+++ 241030/앞에서부터 삭제하기 2/delete-it-from-the-beginning-2.java
@@ -3,31 +3,35 @@ import java.util.Scanner;
 
 public class Main {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		Scanner sc = new Scanner(System.in);
-		int n = sc.nextInt();
-		PriorityQueue<Integer> pq = new PriorityQueue<>();
-		int[] arr = new int[n];
-		double sum = 0;
-		for (int i = 0; i < n; i++) {
-			arr[i] = sc.nextInt();
-			pq.add(arr[i]);
-			sum += arr[i];
-		}
-		double max = Double.MIN_VALUE;
-		for (int i = 0; i < n - 2; i++) {
-			sum -= arr[i];
-			pq.remove(arr[i]);
-			int min = pq.peek();
-			sum -= min;
-			max = Math.max(max, sum / (pq.size() - 1));
-			sum += min;
-		}
-		System.out.printf("%.2f", max);
-		
-		sc.close();
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] arr = new int[n];
 
-	}
+        for (int i = 0; i < n; ++i) {
+        	arr[i] = sc.nextInt();
+        }
 
+        float maxAvg = 0;
+        int sum = 0;
+        PriorityQueue<Integer> pq = new PriorityQueue<>();
+
+        pq.add(-arr[n - 1]);
+        sum += arr[n - 1];
+
+        for (int i = n - 2; i >= 0; --i) {
+            pq.add(-arr[i]);
+            sum += arr[i];
+
+            int minValue = -pq.peek();
+            sum -= minValue;
+            maxAvg = Math.max(maxAvg, (float) sum / (pq.size() - 1));
+
+            sum += minValue; 
+        }
+
+        
+        System.out.printf("%.2f\n", maxAvg);
+        sc.close();
+    }
 }
