fef5d325d9c585fb7d031153a85c777ba06e8b23
YoungEunLee <61973941+cnsejr2@users.noreply.github.com>
2024-10-31 21:05:18 +0900

    Title: 겹치는 숫자가 없는 최대 구간, Time: 258ms, Memory: 21MB, Status: Passed - CodeTree

--- 241031/겹치는 숫자가 없는 최대 구간/max-interval-without-overlapping-numbers.java
+++ 241031/겹치는 숫자가 없는 최대 구간/max-interval-without-overlapping-numbers.java
@@ -18,15 +18,32 @@ public class Main {
 		for (int i = 1; i <= n; i++) {
 			arr[i] = Integer.parseInt(st.nextToken());
 		}
-		int max = 0;
-		int s = 1;
-		for (int i = 2; i <= n; i++) {
-			if (arr[s] == arr[i]) {
-				s++;
-				max = Math.max(max, i - s + 1);
-			}
-		}
-		System.out.println(max);
+		// 가능한 구간 중 최대 크기를 구합니다.
+        int ans = 0;
+        int[] countArray = new int[1000001];
+        // 구간을 잡아봅니다.
+        int j = 0;
+        for(int i = 1; i <= n; i++) {
+            // 같은 숫자가 3개가 되기 전까지 계속 진행합니다.
+            while(j + 1 <= n && countArray[arr[j + 1]] < 1) {
+                countArray[arr[j + 1]]++;
+                j++;
+            }
+            
+            // 현재 구간 [i, j]는 
+            // i를 시작점으로 하는
+            // 가장 긴 구간이므로
+            // 구간 크기 중 최댓값을 갱신합니다.
+            ans = Math.max(ans, j - i + 1);
+
+            // 다음 구간으로 넘어가기 전에
+            // arr[i]에 해당하는 값은 countArray에서 지워줍니다.
+            countArray[arr[i]]--;
+        }
+
+        // 조건을 만족하는 가장 큰 구간의 크기는
+        // [1, 2, 2, 1, 3]로 5가 됩니다.
+        System.out.print(ans);
 		
 		
 	}
