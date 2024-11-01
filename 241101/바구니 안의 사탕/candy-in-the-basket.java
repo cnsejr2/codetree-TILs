9563ff9d5ac58b35799ababf66fabdd64efc41bf
YoungEunLee <61973941+cnsejr2@users.noreply.github.com>
2024-11-01 21:57:35 +0900

    Title: 바구니 안의 사탕, Time: 1159ms, Memory: 57MB, Status: Passed - CodeTree

--- 241101/바구니 안의 사탕/candy-in-the-basket.java
+++ 241101/바구니 안의 사탕/candy-in-the-basket.java
@@ -1,39 +1,44 @@
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Scanner;
 
 public class Main {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		Scanner sc = new Scanner(System.in);
-		
-		int n = sc.nextInt();
-		int k = sc.nextInt();
-		int len = 1;
-		int[] basket = new int[1000001];
-		for (int i = 0; i < n; i++) {
-			int cnt = sc.nextInt();
-			int loc = sc.nextInt();
-			
-			basket[loc] = cnt;
-			len = Math.max(len, loc);
-		}
-		int max = 0;
-		for (int i = 1; i <= len; i++) {
-			int j = 1;
-			int cnt = basket[i];
-			while (j <= k) {
-				if (i - j > 0) { cnt += basket[i - j]; }
-				if (i + j <= len) { cnt += basket[i + j]; }
-				j++;
-			}
-			max = Math.max(cnt, max);
-		}
-		System.out.println(max);
-		
-		sc.close();
+    static final int MAX_N = 100001;
+    static final int MAX_B = 1000001;
 
-	}
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
 
+        int n = sc.nextInt();
+        int k = sc.nextInt();
+        int[] basket = new int[MAX_B];
+        int[] prefix_sum = new int[MAX_B];
+
+        int max_coord = 0;
+        for (int i = 0; i < n; i++) {
+            // 사탕 개수 >> 바구니 좌표
+            int candy = sc.nextInt();
+            int coord = sc.nextInt();
+            basket[coord] += candy;
+            max_coord = Math.max(max_coord, coord);
+        }
+
+        // prefix_sum 계산
+        prefix_sum[0] = basket[0];
+        for (int i = 1; i <= max_coord; i++) {
+            prefix_sum[i] = prefix_sum[i - 1] + basket[i];
+        }
+
+        int max_candy = 0;
+        for (int l = 0; l <= max_coord; l++) {
+            int r = 2 * k + l;
+            // r이 범위를 초과하면 max_coord로 조정
+            if (r > max_coord) r = max_coord;
+
+            if (l == 0) max_candy = Math.max(max_candy, prefix_sum[r]);
+            else max_candy = Math.max(max_candy, prefix_sum[r] - prefix_sum[l - 1]);
+        }
+
+        System.out.println(max_candy);
+        sc.close();
+    }
 }
