a6eb2dda55d3d2d721eb46bd1b7e123b01d06302
YoungEunLee <61973941+cnsejr2@users.noreply.github.com>
2024-11-04 21:20:39 +0900

    Title: 자연수 M/2개의 쌍, Time: 1723ms, Memory: 176MB, Status: Memory Limit Exceed - CodeTree

--- 241104/자연수 M/2개의 쌍/m2-pairs-of-natural-numbers.java
+++ 241104/자연수 M/2개의 쌍/m2-pairs-of-natural-numbers.java
@@ -1,42 +1,38 @@
 import java.util.ArrayList;
-import java.util.List;
 import java.util.Scanner;
 
 public class Main {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		Scanner sc = new Scanner(System.in);
-		
-		int m = sc.nextInt();
-		ArrayList<Long> list = new ArrayList<Long>();
-		for (int i = 0; i < m; i++) {
-			int x = sc.nextInt();
-			long y = sc.nextInt();
-			for (int j = 0; j < x; j++) {
-				list.add(y);
-			}
-		}
-		
-		ArrayList<Long> all = new ArrayList<Long>();
-		all.addAll(list); all.addAll(list);
-		long min  = Long.MAX_VALUE;
-		for (int i = 0; i < list.size(); i++) {
-			long max = 0;
-			for (int j = 0; j < list.size(); j += 2) {
-				long a = all.get(i + j);
-				long b = all.get(i + j + 1);
-				
-				max = Math.max(max, a + b);
-			}
-			min = Math.min(max, min);
-		}
-		System.out.println(min);
-		
-		
-		sc.close();
-		
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
 
-	}
+        int m = sc.nextInt();
+        ArrayList<Long> list = new ArrayList<>();
 
+        // 데이터 입력
+        for (int i = 0; i < m; i++) {
+            int x = sc.nextInt();
+            long y = sc.nextInt();
+            for (int j = 0; j < x; j++) {
+                list.add(y);
+            }
+        }
+
+        long min = Long.MAX_VALUE;
+
+        int n = list.size();
+        for (int i = 0; i < n; i++) {
+            long max = 0;
+            for (int j = 0; j < n / 2; j++) {
+                long a = list.get((i + j * 2) % n);
+                long b = list.get((i + j * 2 + 1) % n);
+
+                max = Math.max(max, a + b);
+            }
+            min = Math.min(max, min);
+        }
+
+        System.out.println(min);
+        sc.close();
+    }
 }
