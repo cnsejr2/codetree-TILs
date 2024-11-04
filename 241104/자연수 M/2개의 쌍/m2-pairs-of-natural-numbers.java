0309d7028f5f39dbbf2497360f173519cdba0748
YoungEunLee <61973941+cnsejr2@users.noreply.github.com>
2024-11-04 21:25:20 +0900

    Title: 자연수 M/2개의 쌍, Time: 1244ms, Memory: 54MB, Status: Passed - CodeTree

--- 241104/자연수 M/2개의 쌍/m2-pairs-of-natural-numbers.java
+++ 241104/자연수 M/2개의 쌍/m2-pairs-of-natural-numbers.java
@@ -1,38 +1,77 @@
-import java.util.ArrayList;
 import java.util.Scanner;
+import java.util.ArrayList;
+import java.util.Collections;
 
-public class Main {
+class Pair implements Comparable<Pair> {
+    int y, x;
+    Pair(int y, int x) {
+        this.y = y;
+        this.x = x;
+    }
+
+    @Override
+    public int compareTo(Pair p) {
+        return this.y - p.y;        // y 기준 오름차순으로 정렬합니다.
+    }
+}
+
+public class Main {    
+    // 변수 선언
+    public static int n;
+    public static ArrayList<Pair> nums = new ArrayList<>();
+    
+    public static int ans;
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-
-        int m = sc.nextInt();
-        ArrayList<Long> list = new ArrayList<>();
-
-        // 데이터 입력
-        for (int i = 0; i < m; i++) {
+        // 입력:
+        n = sc.nextInt();
+        for(int i = 0; i < n; i++) {
             int x = sc.nextInt();
-            long y = sc.nextInt();
-            for (int j = 0; j < x; j++) {
-                list.add(y);
-            }
+            int y = sc.nextInt();
+            nums.add(new Pair(y, x));
         }
 
-        long min = Long.MAX_VALUE;
+        // y 기준으로 오름차순 정렬을 진행합니다.
+        Collections.sort(nums);
 
-        int n = list.size();
-        for (int i = 0; i < n; i++) {
-            long max = 0;
-            for (int j = 0; j < n / 2; j++) {
-                long a = list.get((i + j * 2) % n);
-                long b = list.get((i + j * 2 + 1) % n);
+        // 최선의 전략을 오름차순 정렬 뒤
+        // 가장 작은 수와 큰 수를 하나씩 매칭해주는 것입니다.
+        int li = 0, ri = n - 1;
+        while(li <= ri) {
+            int ly = nums.get(li).y;
+            int lx = nums.get(li).x;
+            int ry = nums.get(ri).y;
+            int rx = nums.get(ri).x;
 
-                max = Math.max(max, a + b);
+            // 답을 갱신합니다.
+            ans = Math.max(ans, ly + ry);
+
+            // 왼쪽 개수가 더 적다면
+            // 왼쪽을 전부 매칭시켜줍니다.
+            if(lx < rx) {
+                // 오른쪽은 lx만큼 개수를 줄여줍니다.
+                nums.set(ri, new Pair(ry, rx - lx));
+
+                // 왼쪽 위치를 한칸 뒤로 옮겨줍니다.
+                li++;
             }
-            min = Math.min(max, min);
-        }
+            // 오른쪽 개수가 더 적다면
+            // 오른쪽을 전부 매칭시켜줍니다.
+            else if(lx > rx) {
+                // 왼쪽은 rx만큼 개수를 줄여줍니다.
+                nums.set(li, new Pair(ly, lx - rx));
 
-        System.out.println(min);
-        sc.close();
+                // 오른쪽 위치를 한칸 앞으로 옮겨줍니다.
+                ri--;
+            }
+            // 개수가 동일하다면
+            // li, ri 위치 모두 옮겨줍니다.
+            else {
+                li++;
+                ri--;
+            }
+        }
+        System.out.print(ans);
     }
 }
