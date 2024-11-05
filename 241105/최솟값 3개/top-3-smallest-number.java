81f7ef99f88a6d4a500b9d4817f523c077052f66
YoungEunLee <61973941+cnsejr2@users.noreply.github.com>
2024-11-05 20:04:07 +0900

    Title: 최솟값 3개, Time: 1000ms, Memory: 25MB, Status: Passed - CodeTree

--- 241105/최솟값 3개/top-3-smallest-number.java
+++ 241105/최솟값 3개/top-3-smallest-number.java
@@ -7,16 +7,16 @@ public class Main {
 		// TODO Auto-generated method stub
 		Scanner sc = new Scanner(System.in);
 		int n = sc.nextInt();
-		PriorityQueue<Integer> pq = new PriorityQueue<>();
+		PriorityQueue<Long> pq = new PriorityQueue<>();
 		for (int i = 0; i < n; i++) {
-			int num = sc.nextInt();
+			Long num = sc.nextLong();
 			pq.add(num);
 			if (pq.size() < 3) {
 				System.out.println(-1);
 			} else {
-				int a = pq.poll();
-				int b = pq.poll();
-				int c = pq.poll();
+				Long a = pq.poll();
+				Long b = pq.poll();
+				Long c = pq.poll();
 				
 				System.out.println(a * b * c);
 				pq.add(a); pq.add(b); pq.add(c);
