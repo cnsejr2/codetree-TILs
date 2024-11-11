f2895b8de3100478710dfc49d03bb3a2a43bc31f
YoungEunLee <61973941+cnsejr2@users.noreply.github.com>
2024-11-11 20:16:01 +0900

    Title: 양쪽 모두 존재하는 구간, Time: 3063ms, Memory: 35MB, Status: Time Limit Exceed - CodeTree

--- 241111/양쪽 모두 존재하는 구간/both-exist-section.java
+++ 241111/양쪽 모두 존재하는 구간/both-exist-section.java
@@ -9,36 +9,36 @@ public class Main {
 		int n = sc.nextInt();
 		int m = sc.nextInt();
 		
-		int[] arr = new int[n];
-		HashMap<Integer, Integer> map = new HashMap<Integer, Integer>();
-		HashMap<Integer, Integer> cnt = new HashMap<Integer, Integer>();
-		for (int i = 0; i < n; i++) {
+		int[] arr = new int[n + 1];
+		int[] map = new int[n + 1];
+		int[] cnt = new int[n + 1];
+		for (int i = 1; i <= n; i++) {
 			arr[i] = sc.nextInt();
-			map.put(arr[i], map.getOrDefault(arr[i], 0) + 1);
+			map[arr[i]]++;
 		}
 		
 		int ans = 0;
-		int j = 0;
-		for (int i = 0; i < n; i++) {
-			while (j < n && cnt.getOrDefault(arr[j], 0) < map.get(arr[j]) - 1) {
-				cnt.put(arr[j], cnt.getOrDefault(arr[j], 0) + 1);
+		int j = 1;
+		for (int i = 1; i <= n; i++) {
+			while (j <= n && cnt[arr[j]] < map[arr[j]] - 1) {
+				cnt[arr[j]]++;
 				j++;
 			}
 			
 			int count = 0;
 			for (int k = 1; k <= m; k++) {
-				if (cnt.getOrDefault(k, 0) > 0) {
+				if (cnt[k] > 0 && map[k] - cnt[k] > 0) {
 					count++;
 				}
 			}
-//			System.out.println(count);
+
 			if (count == m) {
 				ans = Math.max(ans, j - i);
 			}
 			
-			cnt.put(arr[i], cnt.get(arr[i]) - 1);
+			cnt[arr[i]]--;
 		}
-		System.out.println(ans);
+		System.out.println(ans == 0 ? -1 : ans);
 		
 		
 		sc.close();
