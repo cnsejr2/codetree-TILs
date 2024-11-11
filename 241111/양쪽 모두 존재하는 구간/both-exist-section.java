e3db4b53d71449d9ff8a1d0403174949320bf4b1
YoungEunLee <61973941+cnsejr2@users.noreply.github.com>
2024-11-11 21:28:30 +0900

    Title: 양쪽 모두 존재하는 구간, Time: 128ms, Memory: 10MB, Status: Wrong Answer - CodeTree

--- 241111/양쪽 모두 존재하는 구간/both-exist-section.java
+++ 241111/양쪽 모두 존재하는 구간/both-exist-section.java
@@ -1,47 +1,42 @@
 import java.util.HashMap;
+import java.util.Map;
 import java.util.Scanner;
 
 public class Main {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		Scanner sc = new Scanner(System.in);
-		int n = sc.nextInt();
-		int m = sc.nextInt();
-		
-		int[] arr = new int[n + 1];
-		int[] map = new int[n + 1];
-		int[] cnt = new int[n + 1];
-		for (int i = 1; i <= n; i++) {
-			arr[i] = sc.nextInt();
-			map[arr[i]]++;
-		}
-		
-		int ans = Integer.MAX_VALUE;
-		int j = 1;
-		int count = 0;
-		for (int i = 1; i <= n; i++) {
-			while (j <= n && cnt[arr[j]] < map[arr[j]] - 1) {
-				if (cnt[arr[j]] == 0) {
-					count++;
-				}
-				cnt[arr[j]]++;
-				j++;
-			}
-			if (count == m) {
-				ans = Math.min(ans, j - i);	
-			}
-			
-			cnt[arr[i]]--;
-			if (cnt[arr[i]] == 0) {
-				count--;
-			}
-		}
-		System.out.println(ans == Integer.MAX_VALUE ? -1 : ans);
-		
-		
-		sc.close();
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int m = sc.nextInt();
 
-	}
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
 
+        int minLength = Integer.MAX_VALUE;
+        Map<Integer, Integer> countMap = new HashMap<>();
+        int uniqueCount = 0;
+
+        int left = 0;
+        for (int right = 0; right < n; right++) {
+            countMap.put(arr[right], countMap.getOrDefault(arr[right], 0) + 1);
+            if (countMap.get(arr[right]) == 1) {
+                uniqueCount++;
+            }
+
+            while (uniqueCount == m) {
+                minLength = Math.min(minLength, right - left + 1);
+                countMap.put(arr[left], countMap.get(arr[left]) - 1);
+                if (countMap.get(arr[left]) == 0) {
+                    uniqueCount--;
+                }
+                left++;
+            }
+        }
+
+        System.out.println(minLength == Integer.MAX_VALUE ? -1 : minLength);
+
+        sc.close();
+    }
 }
