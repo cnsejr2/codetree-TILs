77bc28ee87e7c1068d55cdff7dccf4814064e39f
YoungEunLee <61973941+cnsejr2@users.noreply.github.com>
2024-11-22 20:24:05 +0900

    Title: 회의실 준비 구현, Time: 1062ms, Memory: 48MB, Status: Passed - CodeTree

---
+++ 241122/회의실 준비 구현/implement-scheduling-meeting-room.java
@@ -0,0 +1,45 @@
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		
+		int n = sc.nextInt();
+		
+		int[][] arr = new int[n][2];
+		for (int i = 0; i < n; i++) {
+			arr[i][0] = sc.nextInt();
+			arr[i][1] = sc.nextInt();
+		}
+		
+		Arrays.sort(arr, new Comparator<int[]>() {
+
+			@Override
+			public int compare(int[] o1, int[] o2) {
+				if (o1[1] == o2[1]) {
+					return o1[0] - o2[0];
+				} else {
+					return o1[1] - o2[1];
+				}
+			}
+		});
+		
+		int end = 0;
+		int cnt = 0;
+		for (int i = 0; i < n; i++) {
+			if (end <= arr[i][0]) {
+				end = arr[i][1];
+				cnt++;
+			}
+		}
+		System.out.println(cnt);
+		
+		sc.close();
+
+	}
+
+}
