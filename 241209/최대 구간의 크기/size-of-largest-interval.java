1938ab4f6d161b8a6c630a97465c5e87f7498cd2
YoungEunLee <61973941+cnsejr2@users.noreply.github.com>
2024-12-09 19:40:21 +0900

    Title: 최대 구간의 크기, Time: 1522ms, Memory: 60MB, Status: Passed - CodeTree

--- 241209/최대 구간의 크기/size-of-largest-interval.java
+++ 241209/최대 구간의 크기/size-of-largest-interval.java
@@ -1,8 +1,4 @@
-public class Main {
-    public static void main(String[] args) {
-        // 여기에 코드를 작성해주세요.
-    }
-}import java.util.Scanner;
+import java.util.Scanner;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.HashSet;
