ad1852348c96fe4ebc26411a3efbc0843f26f12c
YoungEunLee <61973941+cnsejr2@users.noreply.github.com>
2024-12-09 19:41:09 +0900

    Title: 겹치는 선분들, Time: 1197ms, Memory: 55MB, Status: Passed - CodeTree

--- 241209/겹치는 선분들/overlapping-lines.java
+++ 241209/겹치는 선분들/overlapping-lines.java
@@ -1,105 +1,81 @@
 import java.util.Scanner;
 import java.util.ArrayList;
 import java.util.Collections;
-import java.util.HashSet;
 
-class Pair {
-    int x1, x2;
-    public Pair(int x1, int x2) { 
-        this.x1 = x1; 
-        this.x2 = x2; 
-    }
-}
-
-class Tuple implements Comparable<Tuple> { 
-    int x, v, index;
-    public Tuple(int x, int v, int index) { 
-        this.x = x; 
-        this.v = v;
-        this.index = index; 
+class Pair implements Comparable<Pair> { 
+    int first, second;
+    public Pair(int first, int second) { 
+        this.first = first; 
+        this.second = second; 
     }
     @Override
-    public int compareTo(Tuple b) {
-        if(x != b.x) return x - b.x;
-        if(v != b.v) return v - b.v;
-        return index - b.index;
+    public int compareTo(Pair b) {
+        if(first != b.first) return first - b.first;
+        return second - b.second;
     }
 }
 
 public class Main {
-    public static final int MAX_N = 100000;
-    
     // 변수 선언
-    public static int n;
-    public static Pair[] segments = new Pair[MAX_N];
+    public static int n, k;
+    public static int ans;
+    
+    public static ArrayList<Pair> segments = new ArrayList<>();
+    public static ArrayList<Pair> points = new ArrayList<>();
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         // 입력:
         n = sc.nextInt();
-        for(int i = 0; i < n; i++){
-            int x1 = sc.nextInt();
-            int x2 = sc.nextInt();
-            segments[i] = new Pair(x1, x2);
-        }   
-
-        // 각 선분을 두 지점으로 나눠 담은 뒤,
-        // 정렬해줍니다.
-        // 이때 (x좌표, +1-1값, 선분 번호)
-        // 형태로 넣어줍니다.
-        // +1은 시작점
-        // -1은 끝점을 뜻합니다.
-        ArrayList<Tuple> points = new ArrayList<>();
+        k = sc.nextInt();
         
+        // n개의 선분을 만들어줍니다.
+        int curX = 0;
         for(int i = 0; i < n; i++) {
-            int x1 = segments[i].x1;
-            int x2 = segments[i].x2;
-            points.add(new Tuple(x1, +1, i)); // 시작점
-            points.add(new Tuple(x2, -1, i)); // 끝점
+            int dist = sc.nextInt();
+            char cDir = sc.next().charAt(0);
+            if(cDir == 'L') {
+                segments.add(new Pair(curX - dist, curX));
+                curX -= dist;
+            }
+            else {
+                segments.add(new Pair(curX, curX + dist));
+                curX += dist;
+            }
+        }
+        // 주어진 좌표의 범위가 큰 경우에는
+        // 각 선분을 두 지점으로 나눠서
+        // +1, -1로 담은 뒤,
+        // 정렬해줍니다.
+        for(int i = 0; i < n; i++) {
+            int x1 = segments.get(i).first;
+            int x2 = segments.get(i).second;
+            points.add(new Pair(x1, +1)); // 시작점
+            points.add(new Pair(x2, -1)); // 끝점
         }
 
         // 정렬을 진행합니다.
         Collections.sort(points);
 
-        // 각 점을 순서대로 순회합니다.
-        // 등장하고 아직 사라지지 않은
-        // 선분을 hashset으로 관리합니다.
-        HashSet<Integer> segs = new HashSet<>();
-
-        int ans = 0; // 가장 큰 구간의 크기를 출력합니다.
-        int startX = -1; // 현재 합쳐진 구간의 시작 x값을 기록해줍니다.
+        // 각 위치에 적혀있는 숫자들의 합을 구하면
+        // 매 순간마다 겹치는 구간의 횟수가 구해집니다.
+        // 이 때 k 이상인 경우에 대해 구간의 합을 구하면 됩니다.
+        int sumVal = 0;
         for(int i = 0; i < 2 * n; i++) {
-            int x = points.get(i).x;
-            int v = points.get(i).v;
-            int index = points.get(i).index;
+            int x = points.get(i).first;
+            int v = points.get(i).second;
 
-            // 시작점인 경우입니다.
-            if(v == +1) {
-                // 남아있는 선분이 없다면
-                // 합쳐진 구간의 시작이므로
-                // startX값을 갱신해줍니다.
-                if((int) segs.size() == 0)
-                    startX = x;    
-                
-                // 해당 선분 번호를 hashset에 넣어줍니다.
-                segs.add(index);
+            // k개 이상 겹치는 구간이라면
+            // 해당 구간의 길이를 더해줍니다.
+            if(sumVal >= k) {
+                int prevX = points.get(i - 1).first;
+                ans += x - prevX;
             }
 
-            // 끝점인 경우입니다.
-            else {
-                // 해당 선분을 제거합니다.
-                segs.remove(index);
-
-                // 남아있는 선분이 없다면
-                // 합쳐진 구간의 끝이므로
-                // 답을 갱신해줍니다.
-                if((int) segs.size() == 0) {
-                    int endX = x;
-                    ans = Math.max(ans, endX - startX);
-                }
-            }
+            // 적혀있는 가중치를 전부 더해줍니다.
+            sumVal += v;
         }
         
-        System.out.println(ans);
+        System.out.print(ans);
     }
 }
