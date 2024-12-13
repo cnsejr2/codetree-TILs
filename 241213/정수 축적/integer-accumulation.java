1f21a8c3a3c7cf2880785b745a633e5366480b50
YoungEunLee <61973941+cnsejr2@users.noreply.github.com>
2024-12-13 20:23:09 +0900

    Title: 정수 축적, Time: 398ms, Memory: 23MB, Status: Passed - CodeTree

--- 241213/정수 축적/integer-accumulation.java
+++ 241213/정수 축적/integer-accumulation.java
@@ -8,24 +8,29 @@ public class Main {
 	static int[][] dp;
 	static int[] taskValues;
 	
+	// idx : 현재 작업의 인덱스
+	// cur : 현재 피로도
 	private static int getMaxValue(int idx, int cur) {
+		// 모든 작업을 다 수행한 경우
 		if (idx == totalTask) {
+			// 피로도가 0이라면 0을 반환하고, 그렇지 않다면 매우 작은 값을 반환
 			return cur == 0 ? 0 : INF;
 		}
-		
+		// 이미 계산된 값이 있다면 그 값을 반환
 		if (dp[idx][cur] != -1) {
 			return dp[idx][cur];
 		}
 		
-		int result = INF;
-		if (cur > 0) {
+		int result = INF; // 초기값을 매우 작은 값으로 설정
+		if (cur > 0) {  // Option 1: 피로도가 0보다 큰 경우 휴식을 취합니다.
 			if (idx + cur <= totalTask) {
 				result = Math.max(result, getMaxValue(idx + cur, 0));
 			}
 		} else {
+			// Option 2: 피로도를 증가시키지 않고 다음 작업으로 이동합니다.
 			result = Math.max(result, getMaxValue(idx + 1, 0));
 		}
-		
+		// Option 3: 현재 작업을 수행하고 피로도를 증가시킵니다.
 		if (cur + 1 <= maxFatigue) {
 			result = Math.max(result, getMaxValue(idx + 1, cur + 1) + taskValues[idx]);
 		}
