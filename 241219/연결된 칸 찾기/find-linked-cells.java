d27bda83f29e1e40e45c72c20274a08af4423f0c
YoungEunLee <61973941+cnsejr2@users.noreply.github.com>
2024-12-19 20:49:23 +0900

    Title: 연결된 칸 찾기, Time: 105ms, Memory: 9MB, Status: Wrong Answer - CodeTree

---
+++ 241219/연결된 칸 찾기/find-linked-cells.java
@@ -0,0 +1,66 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.StringTokenizer;
+
+public class Main {
+	
+	static int n, cnt;
+	static int[] dx = { -1, 0, 1, 0};
+	static int[] dy = { 0, -1, 0, 1};
+	static int[][] mat;
+	static boolean[][] visit;
+	static ArrayList<Integer> list = new ArrayList<Integer>();
+
+	public static void main(String[] args) throws IOException {
+		// TODO Auto-generated method stub
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		
+		n = Integer.parseInt(st.nextToken());
+		mat = new int[n][n];
+		visit = new boolean[n][n];
+		for (int i = 0; i < n; i++) {
+			st = new StringTokenizer(br.readLine());
+			for (int j = 0; j < n; j++) {
+				mat[i][j] = Integer.parseInt(st.nextToken());
+			}
+		}
+		
+		for (int i = 0; i < n; i++) {
+			for (int j = 0; j < n; j++) {
+				if (mat[i][j] == 1 && !visit[i][j]) {
+					cnt = 0;
+					dfs(i, j);
+					list.add(cnt);
+				}
+			}
+		}
+	
+		
+		System.out.println(list.size());
+		for (int i = 0; i < list.size(); i++) {
+			System.out.println(list.get(i));
+		}
+
+	}
+
+	private static void dfs(int x, int y) {
+		// TODO Auto-generated method stub
+		visit[x][y] = true;
+		cnt++;
+		for (int i = 0; i < 4; i++) {
+			int nx = x + dx[i];
+			int ny = y + dy[i];
+			
+			if (nx >= 0 && nx < n && ny >= 0 && ny < n) {
+				if (mat[nx][ny] == 1 && !visit[nx][ny]) {
+					dfs(nx, ny);
+				}
+			}
+		}
+		
+	}
+
+}
